11adafc852fc95a4d5618abef27f1f807cfb6d59
C5rsdMat1X5 <matias.henriquez.fco@gmail.com>
2026-06-15 17:22:08 -0400

    Updated build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ GLFW_LIBS=$(pkg-config --libs glfw3)
 CXXFLAGS="-std=c++17 -O3 -march=native -ffast-math -fomit-frame-pointer -flto"
 FRAMEWORKS="-framework Metal -framework QuartzCore -framework Foundation -framework AppKit -framework IOKit"
 
+[ -d "build" ] || mkdir -p "build"
 c++ $CXXFLAGS $GLFW_CFLAGS -c src/main.cpp     -o build/main.o     &
 c++ $CXXFLAGS $GLFW_CFLAGS -c src/metal_sim.mm -o build/metal_sim.o &
 wait
