55401a8e8905ec4331ef5dd17bb0751a901b9f67
C5rsdMat1X5 <matias.henriquez.fco@gmail.com>
2026-06-14 22:35:09 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,12 +6,10 @@ GLFW_LIBS=$(pkg-config --libs glfw3)
 CXXFLAGS="-std=c++17 -O3 -march=native -ffast-math -fomit-frame-pointer -flto"
 FRAMEWORKS="-framework Metal -framework QuartzCore -framework Foundation -framework AppKit -framework IOKit"
 
-# Compilar TUs en paralelo
 c++ $CXXFLAGS $GLFW_CFLAGS -c src/main.cpp     -o build/main.o     &
 c++ $CXXFLAGS $GLFW_CFLAGS -c src/metal_sim.mm -o build/metal_sim.o &
 wait
 
-# Linkear
 c++ $CXXFLAGS build/main.o build/metal_sim.o \
     $FRAMEWORKS $GLFW_LIBS \
     -o build/circles
